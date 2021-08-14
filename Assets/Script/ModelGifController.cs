818a14ff850ebe0d96f1f81408874beab8a81899
AnnaCCCCo <annawang0715@gmail.com>
2021-08-14 21:22:34 +0100

    2021/08/14 second submit

---
+++ Assets/Script/ModelGifController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ModelGifController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator rotation;
+
+    void Start()
+    {
+        rotation = this.gameObject.GetComponent<Animator>();
+        rotation.speed = 0;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rotation.speed = 1;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rotation.speed = 0;
+    }
+}
